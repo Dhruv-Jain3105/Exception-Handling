bec046f3eeb4fd7a4656dfe0870944574ff3088b
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-09-24 15:46:27 +0530

    Create division by 0.cpp

---
+++ division by 0.cpp
@@ -0,0 +1,34 @@
+// Name: Dhruv Jain
+// PRN: 24070123042
+// Branch: EnTC A2
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a, b, ans;
+
+    cout << "Enter two numbers: " << endl;
+    cin >> a >> b;
+
+    try {
+        if(b == 0) {
+            throw b;
+        }
+        ans = a / b;
+        cout << "Division is: " << ans;
+    }
+    catch(float) {
+        cout << "Denominator cannot be zero.";
+    }
+    return 0;
+}
+
+/*
+SAMPLE OUTPUT ::
+
+Enter two numbers:
+5
+0
+Denominator cannot be zero.
+*/
